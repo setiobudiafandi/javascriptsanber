d0b77c7a3b04f1032358dbf23b35978334f057d4
setiobudiafandi <setiobudi71@gmail.com>
2024-06-28 05:46:04 +0700

    ubah file

--- contoh.js
+++ contoh.js
@@ -1,2 +1,8 @@
-console.log("hallo");
-console.log("test");
+//console.log("hallo");
+
+const prompt = require('prompt-sync')({sigint : true});
+
+num1 = prompt("masukkan angka pertama : ");
+num2 = prompt("kini angka ke-2 : ");
+num3 = Number(num1) + Number(num2);
+console.log("Hasil penjumlahannya : "+num3);
